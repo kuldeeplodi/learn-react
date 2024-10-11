ae5be78b6fba5c72ec49950288ca4b1bbb8202ba
kuldeeplodi <kuldeeplodi@gmail.com>
2024-10-11 23:11:35 +0530

    add card

--- 03tailwind/src/App.jsx
+++ 03tailwind/src/App.jsx
@@ -1,9 +1,13 @@
+import Card from "./component/Card";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline bg-green-400">
+      <h1 className="text-3xl font-bold underline bg-green-400 ">
         Hello world!
       </h1>
+      <Card username="kuldeep"></Card>
+      <Card username="jyoti"></Card>
+      <Card username="sanjana"></Card>
     </>
   );
 }
