7075e586a7b19f74dbb9ddbb84292f59aa32f0f4
kuldeeplodi <kuldeeplodi@gmail.com>
2024-10-18 18:16:32 +0530

    currency converter

--- 06currencyconverter/src/components/InputBox.jsx
+++ 06currencyconverter/src/components/InputBox.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useId } from "react";
 
 function InputBox({
   label,
@@ -9,14 +9,17 @@ function InputBox({
   selectCurrency = "usd",
   amountDisable = false,
   currencyDisable = false,
-
   className = "",
 }) {
+  const amountId = useId();
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className="w-1/2">
-        <label className="text-black/40 mb-2 inline-block">{label}</label>
+        <label htmlFor={amountId} className="text-black/40 mb-2 inline-block">
+          {label}
+        </label>
         <input
+          id={amountId}
           className="outline-none w-full bg-transparent py-1.5"
           type="number"
           placeholder="Amount"
@@ -35,12 +38,13 @@ function InputBox({
           onChange={(e) => {
             onCurrencyChange && onCurrencyChange(e.target.value);
           }}
+          disabled={currencyDisable}
         >
-          {currencyOption.map((currency) => {
+          {currencyOption.map((currency) => (
             <option key={currency} value={currency}>
               {currency}
-            </option>;
-          })}
+            </option>
+          ))}
         </select>
       </div>
     </div>
--- 06currencyconverter/src/hooks/useCurrencyInfo.js
+++ 06currencyconverter/src/hooks/useCurrencyInfo.js
@@ -1,22 +1,15 @@
 import { useEffect, useState } from "react";
 function useCurrencyInfo(currency) {
-  const [data, setdata] = useState();
+  const [data, setdata] = useState({});
   useEffect(() => {
     fetch(
       `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`
-    );
-  })
-    .then((res) => {
-      res.json();
-    })
-    .then(
-      (res) => {
-        setdata(res[currency]);
-        console.log(data);
-      },
-      [currency]
-    );
-  console.log(data);
+    )
+      .then((res) => res.json())
+      .then((res) => setdata(res[currency]));
+    console.log(data);
+  }, [currency]);
+  // console.log(data);
   return data;
 }
 
